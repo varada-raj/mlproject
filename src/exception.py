cfb325c235367a6d59c6e1ece14d89a81cbca227
KamisettyVaradaraj-powerschool <kamisetty.varadaraj@powerschool.com>
2025-12-08 00:11:45 +0530

    data ingestion part

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 # thisfile is used to handle error handling
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,4 +17,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
